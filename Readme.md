ab4728a56eecdd79332bd1d059e3cf393444082a
willhaines <will@haines.cc>
2023-10-31 15:10:45 -0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+Photo Album Exercise
+====================
+
+Use this repository as an example for getting started with GitHub Actions and/or
+OpenShift. It contains:
+
+1. Container sources to produce a statically-generated photo album website.
+2. GitHub Actions workflows that build the image and host it with GitHub Packages.
+3. Kubernetes manifests that host the built image in an OpenShift cluster.
+
+This repo is not meant to stand alone, see [these docs]() for a detailed guide.
---
+++ sources/sigal.conf.py
@@ -0,0 +1,21 @@
+# https://raw.githubusercontent.com/saimn/sigal/main/src/sigal/templates/sigal.conf.py
+
+# ---------------------
+# General configuration
+# ---------------------
+
+# Source directory. Can be set here or as the first argument of the `sigal
+# build` command
+source = 'pictures'
+
+# Destination directory. Can be set here or as the second argument of the
+# `sigal build` command (default: '_build')
+# destination = '_build'
+
+# Theme :
+# - colorbox (default), galleria, photoswipe, or the path to a custom theme
+# directory
+theme = 'galleria'
+
+# Theme for galleria (https://galleriajs.github.io/themes/)
+# galleria_theme = 'classic'
